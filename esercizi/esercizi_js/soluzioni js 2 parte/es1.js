f0e2cf2cc7bd65265a1a140042217430823c5fc1
Giancarlo Ruffo <ruffo@di.unito.it>
2023-04-26 11:23:31 +0200

    sych all

---
+++ esercizi/esercizi_js/soluzioni js 2 parte/es1.js
@@ -0,0 +1,15 @@
+function char_count(str, letter) 
+{
+ let letter_Count = 0;
+ for (let position = 0; position < str.length; position++) 
+ {//scorro carattere per carattere 
+
+    if (str.charAt(position) === letter) 
+      {//confronto il carattere in una data poszione della stringa con la lettera che cerco
+      letter_Count += 1;
+      }
+  }
+  return letter_Count;
+}
+
+console.log(char_count('prova', 'p'));
