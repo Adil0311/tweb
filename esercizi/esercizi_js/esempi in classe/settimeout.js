5f11207e707e7e67073c7aa0bc3e250a98f53372
Giancarlo Ruffo <ruffo@di.unito.it>
2023-04-28 12:22:46 +0200

    update esercizi js

---
+++ esercizi/esercizi_js/esempi in classe/settimeout.js
@@ -0,0 +1,6 @@
+const onesec= setTimeout(()=> {
+    console.log('hey') ; // after 1s
+    }, 5000) ;
+    
+console.log('hi') ;
+    
---
+++ esercizi/esercizi_js/esempi in classe/synchcallback1.js
@@ -0,0 +1,15 @@
+function logQuote(quote) {
+    console.log(quote);
+    }
+
+function logString(string) {
+    console.log('questa funzione stampa solo '+ string)
+}
+    
+function createQuote(quote, callback) {
+const myQuote= `Like I always say, '${quote}'`;
+callback(myQuote);
+}
+    
+createQuote("JavaScript rocks!", logQuote);
+createQuote("JavaScript rocks!", logString);
