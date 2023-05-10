5ceb4800dc7b0275b06ab94bc12e19ebaa27f497
Giancarlo Ruffo <ruffo@di.unito.it>
2023-05-10 11:11:02 +0200

    synch

--- esercizi/esercizi_js_dom/change.js
+++
@@ -1,8 +0,0 @@
-function changeText() {
-	let newtext = document.getElementById("myinput");
-	document.getElementById("myspan").innerHTML = newtext.value;		
-}	
-
-window.onload = function () {
-    document.getElementById("btn1").onclick = changeText;
-}
--- esercizi/esercizi_js_dom/link.js
+++
@@ -1,8 +0,0 @@
-window.onload = function () {
-    let links = ["http://slashdot.org/", "http://www.thinkgeek.com/", "http://despair.com/", 
-    "http://www.redbubble.com/", "http://googleresearch.blogspot.com/"];
-
-    let index = parseInt(Math.random() * links.length);
-
-    document.getElementById("lnk").href=links[index];
-}
